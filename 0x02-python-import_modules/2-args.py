5613a026e519fb00f8e70689a544bc4d5c30c8bb
utuedey <utuedeyejosph@gmail.com>
2022-09-23 03:22:53 +0000

    Add shebang to 5-variable_load

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
         print("{} arguments".format(len(argv)-1))
-    elif len(argv) >= 2:
-        if len(argv) == 2:
-            print("{} argument:".format(len(argv)-1))
-        else:
-            print("{} arguments:".format(len(argv)-1))
-        for arg in range(1, len(argv)):
-            print("{}: {}".format(arg, argv[arg]))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv)-1))
+    else:
+        print("{} arguments:".format(len(argv)-1))
+    for arg in range(1, len(argv)):
+        print("{}: {}".format(arg, argv[arg]))
