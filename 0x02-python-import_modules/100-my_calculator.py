cc9c673346f505815eff95fba490e8b2da56c646
utuedey <utuedeyejosph@gmail.com>
2022-09-23 04:54:51 +0000

    Add 4-hidden_discovery file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
+    
     num_args = len(sys.argv)
     if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
