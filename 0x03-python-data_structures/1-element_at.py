b567a02b3ced85ab083cffbd5adc9fcaeb8f1885
utuedey <utuedeyejosph@gmail.com>
2022-09-24 15:28:25 +0000

    modify 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if int(idx) < 0:
+    if idx < 0:
         return None
-    elif int(idx) > len(my_list):
+    elif idx > (len(my_list) - 1):
         return None
     else:
         return my_list[int(idx)]
