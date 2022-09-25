f590671b885f1d51f423dcbfd047ad3c709b7766
utuedey <utuedeyejosph@gmail.com>
2022-09-25 17:25:04 +0000

    Add: 13-is_palindrome modify 9-max_integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_number = 0
     if len(my_list) == 0:
         return None
     else:
-        for number in my_list:
-            if number > max_number:
-                max_number = number
-    return max_number
+        max_number = my_list[0]
+        for number in range(len(my_list)):
+            if my_list(number) > max_number:
+                max_number = my_list(number)
+        return max_number
