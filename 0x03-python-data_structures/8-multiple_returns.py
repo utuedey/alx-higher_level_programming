00c7da15ed92716c48c92942b129b2ac047fe003
utuedey <utuedeyejosph@gmail.com>
2022-09-25 22:50:52 +0000

    Add: 6-print_matrix_integer

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-       a, b = 0, None
+        a, b = 0, None
     else:
         a, b = len(sentence), sentence[0]
     return a, b
