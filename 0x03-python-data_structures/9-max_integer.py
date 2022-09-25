f644612057f8718e8291159cfc8e04e2591492f7
utuedey <utuedeyejosph@gmail.com>
2022-09-25 17:00:09 +0000

    modify 9-max_integer Add: 11-delete_at

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_number = 0
-    for number in my_list:
-        if number > max_number:
-            max_number = number
+    if len(my_list) == 0:
+        return None
+    else:
+        for number in my_list:
+            if number > max_number:
+                max_number = number
     return max_number
