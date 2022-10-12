077c918b8e7059a89a4e28506e9b2482647d6636
utuedey <utuedeyejosph@gmail.com>
2022-10-12 01:55:44 -0700

    Add 2 advance task files

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Magic class from a given ByteCode.
+"""
+
+
+import math
+
+
+class MagicClass:
+    """Represent the MagicClass."""
+    def __init__(self, radius=0):
+        """Initialization of the data."""
+        self._MagicClass__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """Calculate the area."""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculate the circumference."""
+        return 2 * math.pi * self._MagicClass__radius
