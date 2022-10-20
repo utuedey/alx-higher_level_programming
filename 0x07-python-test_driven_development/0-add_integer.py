399faaad5a4a8ce09a8eb6452f8d90d583c194aa
utuedey <utuedeyejosph@gmail.com>
2022-10-20 02:27:08 -0700

    Add:6-max_integer_test.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,7 +18,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    if float(a) or float(b):
+    if type(a) is float:
         a = int(a)
+    if type(b) is float:
         b = int(b)
     return a + b
