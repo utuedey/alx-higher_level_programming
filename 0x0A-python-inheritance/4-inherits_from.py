f1797fd1ab2682e837194bd42cc51f03b6fb8280
utuedey <utuedeyejosph@gmail.com>
2022-10-24 13:50:47 -0700

    Add 2 mandatory task file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module 4-inherits_from.py
+"""
+
+
+def inherits_from(obj, a_class):
+    """Determines if obj is an instance of a class that
+    inherited from a_class.
+    Args:
+        - obj: object to look at
+        - a_class: class to evaluate
+    Returns: True or False
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module 5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """Represent an empty class BaseGeometry"""
+    pass
