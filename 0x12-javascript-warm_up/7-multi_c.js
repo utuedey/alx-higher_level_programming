681741cf1b0ee3227d470a56f30963c66edb8108
utuedey <utuedeyejosph@gmail.com>
2022-12-20 00:29:55 -0800

    remove trailing spaces

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');  
+  console.log('Missing number of occurrences');
 } else {
   const x = Number(process.argv[2]);
   let i = 0;
