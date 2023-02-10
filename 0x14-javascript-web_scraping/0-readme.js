88bbf5058ecb46220d6e9d841ae79554eb286749
utuedey <utuedeyejosph@gmail.com>
2023-02-10 12:10:00 -0800

    indentation of two

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
 });
