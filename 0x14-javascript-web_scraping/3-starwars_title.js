43d57e970569d01dfb9b7140a43692b7bf3861f7
utuedey <utuedeyejosph@gmail.com>
2023-02-10 13:23:58 -0800

    Add 4-starwars_count.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
-    console.error(error);	
+    console.error(error);
   }
   console.log(JSON.parse(body).title);
 });
