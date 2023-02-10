266e42c023c781c57f7ffe61687c6cf165e78965
utuedey <utuedeyejosph@gmail.com>
2023-02-10 12:56:27 -0800

    Add 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);	
+  }
+  console.log(JSON.parse(body).title);
+});
