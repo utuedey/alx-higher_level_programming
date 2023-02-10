13891cbb9f0148fca8c54a6bf09dffc6dadce59b
utuedey <utuedeyejosph@gmail.com>
2023-02-10 12:34:53 -0800

    change write to writeFile

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 constant fs = require('fs');
 
 try {
-  fs.write(process.argv[2], process.argv[3]), 'utf-8', function (err, result) {
+  fs.writeFile(process.argv[2], process.argv[3]), 'utf-8', function (err, result) {
 if (err) console.log(err); });
 } catch (err) {
   console.log(err);
